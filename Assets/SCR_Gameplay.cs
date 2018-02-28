04f1d917ec21d69670017dfc319413751d5be981
Canh <daongoccanh@gmail.com>
2018-02-28 23:09:52 +0700

    - basic scene scripts

---
+++ Assets/SCR_Gameplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SCR_Gameplay : MonoBehaviour {
+	public Text question;
+	public Text score;
+	public Text resultScore;
+	public Text resultBest;
+	public Text gameOver;
+
+	// Use this for initialization
+	void Start () {
+		resultScore.enabled = false;
+		resultBest.enabled = false;
+		gameOver.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/SCR_Menu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SCR_Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnAddition () {
+		SceneManager.LoadScene ("SCN_Gameplay");
+	}
+
+	public void OnSubtraction () {
+		SceneManager.LoadScene ("SCN_Gameplay");
+	}
+
+	public void OnMultiplication () {
+		SceneManager.LoadScene ("SCN_Gameplay");
+	}
+
+	public void OnDivision () {
+		SceneManager.LoadScene ("SCN_Gameplay");
+	}
+
+	public void OnExponentiation () {
+		SceneManager.LoadScene ("SCN_Gameplay");
+	}
+
+	public void OnSquareRoot () {
+		SceneManager.LoadScene ("SCN_Gameplay");
+	}
+}
