40d61e8ac2cfb1fb3db15e89d83511e6a73048cb
Canh <daongoccanh@gmail.com>
2018-03-06 23:53:29 +0700

    - 3d buttons

---
+++ Assets/SCR_Button.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SCR_Button : Button {
+	protected float[] bottoms;
+	protected float[] tops;
+
+	protected override void Start () {
+		bottoms = new float[transform.childCount];
+		tops = new float[transform.childCount];
+
+		for (int i = 0; i < transform.childCount; i++) {
+			Transform child = transform.GetChild (i);
+			RectTransform rectTransform = child.GetComponent<RectTransform> ();
+
+			bottoms[i] = rectTransform.offsetMin.y;
+			tops[i] = rectTransform.offsetMax.y;
+		}
+	}
+
+	protected override void DoStateTransition (Selectable.SelectionState state, bool instant)
+	{
+		base.DoStateTransition (state, instant);
+
+		if (tops != null) {
+			for (int i = 0; i < transform.childCount; i++) {
+				float bottom = bottoms [i];
+				float top = tops [i];
+
+				if (state == Selectable.SelectionState.Pressed) {
+					bottom -= 20;
+					top -= 20;
+				}
+
+				Transform child = transform.GetChild (i);
+				RectTransform rectTransform = child.GetComponent<RectTransform> ();
+
+				rectTransform.offsetMin = new Vector2 (rectTransform.offsetMin.x, bottom);
+				rectTransform.offsetMax = new Vector2 (rectTransform.offsetMax.x, top);
+			}
+		}
+	}
+}
--- Assets/SCR_Menu.cs
+++ Assets/SCR_Menu.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SCR_Menu : MonoBehaviour {
-
-	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
 	}
 
 	public void OnAddition () {
