19fbcf0ecc780ee619248cb7a3997663bc5dc1af
canhdao <daongoccanh@gmail.com>
2018-03-06 16:42:35 +0700

    + addition gameplay

--- Assets/SCR_Gameplay.cs
+++ Assets/SCR_Gameplay.cs
@@ -5,8 +5,15 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SCR_Gameplay : MonoBehaviour {
+	public Text question;
+	
+	private int first = 1;
+	private int second = 1;
+	private int result = 2;
+	
 	// Use this for initialization
 	void Start () {
+		question.text = first + " + " + second + "\n= " + result;
 	}
 	
 	// Update is called once per frame
@@ -14,11 +21,29 @@ public class SCR_Gameplay : MonoBehaviour {
 		
 	}
 	
+	private void NextQuestion () {
+		first = Random.Range (0, 10);
+		second = Random.Range (0, 10);
+		result = first + second + Random.Range (-1, 2);
+		
+		question.text = first + " + " + second + "\n= " + result;
+	}
+	
 	public void OnRight () {
-		SceneManager.LoadScene ("SCN_GameOver");
+		if (first + second == result) {
+			NextQuestion ();
+		}
+		else {
+			SceneManager.LoadScene ("SCN_GameOver");
+		}
 	}
 	
 	public void OnWrong () {
-		SceneManager.LoadScene ("SCN_GameOver");
+		if (first + second != result) {
+			NextQuestion ();
+		}
+		else {
+			SceneManager.LoadScene ("SCN_GameOver");
+		}
 	}
 }
