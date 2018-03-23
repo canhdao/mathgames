bb7cac353691312e891e8aff9ff308b6107b6a34
Canh <daongoccanh@gmail.com>
2018-03-23 22:56:44 +0700

    - update addition mode

--- Assets/SCR_Gameplay.cs
+++ Assets/SCR_Gameplay.cs
@@ -14,66 +14,89 @@ public class SCR_Gameplay : MonoBehaviour {
 	
 	public RectTransform timeRect;
 	
-	private int first = 1;
-	private int second = 1;
-	private int result = 2;
+	private int first = 0;
+	private int second = 0;
+	private int result = 0;
+
+	private int lastFirst = 0;
+	private int lastSecond = 0;
 	
 	private float time = 0;
 
-	// Use this for initialization
-	void Start () {
-		SCR_Profile.Load ();
+	void Start() {
+		SCR_Profile.Load();
 		
 		s_score = 0;
-		question.text = first + " + " + second + "\n= " + result;
+
+		NextQuestion();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update() {
 		if (time < TIMEOUT) {
 			time += Time.deltaTime;
 			if (time >= TIMEOUT) time = TIMEOUT;
 			timeRect.anchorMax = new Vector2((TIMEOUT - time) / TIMEOUT, timeRect.anchorMax.y);
 		}
 		else {
-			SceneManager.LoadScene ("SCN_GameOver");
+			SceneManager.LoadScene("SCN_GameOver");
 		}
 	}
 	
-	private void IncreaseScore () {
+	private void IncreaseScore() {
 		s_score++;
-		score.text = s_score.ToString ();
-		SCR_Profile.UpdateBestScore (s_score);
+		score.text = s_score.ToString();
+		SCR_Profile.UpdateBestScore(s_score);
 	}
 	
-	private void NextQuestion () {
-		first = Random.Range (1, 10);
-		second = Random.Range (1, 10);
-		result = first + second + Random.Range (-1, 2);
-		
+	private void NextQuestion() {
+		while (first == lastFirst && second == lastSecond) {
+			first = Random.Range (1, 10);
+			second = Random.Range (1, 10);
+		}
+
+		lastFirst = first;
+		lastSecond = second;
+
+		int r = Random.Range(0, 2);
+
+		if (r == 0) {
+			// right answer
+			result = first + second;
+		}
+		else {
+			// wrong answer
+			r = Random.Range(0, 2);
+
+			if (r == 0) {
+				r = -1;
+			}
+
+			result = first + second + r;
+		}
+
 		question.text = first + " + " + second + "\n= " + result;
 		
 		time = 0;
 		timeRect.anchorMax = new Vector2(1, timeRect.anchorMax.y);
 	}
 	
-	public void OnRight () {
+	public void OnRight() {
 		if (first + second == result) {
-			IncreaseScore ();
-			NextQuestion ();
+			IncreaseScore();
+			NextQuestion();
 		}
 		else {
-			SceneManager.LoadScene ("SCN_GameOver");
+			SceneManager.LoadScene("SCN_GameOver");
 		}
 	}
 	
-	public void OnWrong () {
+	public void OnWrong() {
 		if (first + second != result) {
-			IncreaseScore ();
-			NextQuestion ();
+			IncreaseScore();
+			NextQuestion();
 		}
 		else {
-			SceneManager.LoadScene ("SCN_GameOver");
+			SceneManager.LoadScene("SCN_GameOver");
 		}
 	}
 }
